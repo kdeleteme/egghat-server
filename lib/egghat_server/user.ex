4f7bba51a5d83e7fee3c6cb4a11ded7d21fb6ae9
Kent Delante <kdeleteme@tutanota.com>
2020-09-06 20:00:13 +0800

    Add user schema

---
+++ lib/egghat_server/user.ex
@@ -0,0 +1,22 @@
+defmodule EgghatServer.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "users" do
+    field :display_name, :string
+    field :email, :string
+    field :id, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:id, :username, :email, :display_name])
+    |> validate_required([:id, :username, :email])
+  end
+end
