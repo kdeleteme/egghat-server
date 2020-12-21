d7ba27adfe75814f4dd23075d4a74ffe9a59cd0c
Kent Delante <kdeleteme@tutanota.com>
2020-12-21 22:35:02 +0800

    Update packages

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule EgghatServer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 2.0"},
       {:phoenix, "~> 1.5.4"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
@@ -46,7 +47,7 @@ defmodule EgghatServer.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
       # Manually added libraries
       {:phx_gen_auth, "~> 0.5", only: [:dev], runtime: false}
     ]
